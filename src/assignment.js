05bc933a000701153d5755a20b46cb46857ac6cc
Okhuarobo Osayuki <108624425+okhuarobo-osayuki@users.noreply.github.com>
2022-07-14 22:39:55 +0000

    check challenge 1 & 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,10 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sumOfNum = arrayOfNumbers.reduce((a, b) => a + b, 0);
+    return sumOfNum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +29,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let evenNum = arrayOfNumbers.filter(num => num % 2 == 0)
+    return evenNum.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
